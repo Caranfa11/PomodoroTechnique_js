1bf9d993cc02070321c22b17a6e093301fa932c9
Sistemas1 Europa <luiscaranfa1@gmail.com>
2023-07-07 11:03:24 -0400

    Script type change on html to module, and left-view div was commited, for aplly them on leftView.js imported on index.js

---
+++ js_templates/index.js
@@ -0,0 +1,3 @@
+import finalView from "./leftView.js"
+
+document.getElementById("left-view").innerHTML=finalView;
---
+++ js_templates/leftView.js
@@ -0,0 +1,50 @@
+import timer from "../js/timer.js"
+
+var finalView = `
+<div class="left__background">
+    <div class="backgroundCircle">
+        <div class="circle__content">
+            <div class="circle__timer">
+                <spam class="timer" id="timer">00:00</spam>
+            </div>
+        </div>
+    </div>
+    <div class="pomodoros">
+        <div class="pomodoros__check">
+            <div class="pomodoros__check--on">
+            </div>
+        </div>
+        <div class="pomodoros__check">
+            <div class="pomodoros__check--off">
+            </div>
+        </div>
+        <div class="pomodoros__check">
+            <div class="pomodoros__check--off">
+            </div>
+        </div>
+        <div class="pomodoros__check">
+            <div class="pomodoros__check--off">
+            </div>
+        </div>
+        <div class="pomodoros__check">
+            <div class="pomodoros__check--off">
+            </div>
+        </div>
+    </div>
+    <div class="buttonsArea">
+        <div class="buttonsArea__box">
+            <button class="hide background__button" id="pause">
+                <i class="icon fas fa-pause"></i>
+            </button>
+            <button class="background__button" id="play">
+                <i class="icon fas fa-play"></i>
+            </button>
+            <button class="background__button" id="reset">
+                <i class="icon fas fa-undo-alt"></i>
+            </button>
+        </div>
+    </div>
+</div>
+`;
+
+export default finalView
