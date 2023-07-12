b880346ec3f23fead3d33a695c83b894950de30f
Caranfa11 <luiscaranfa1@gmail.com>
2023-07-11 22:02:07 -0400

    Dark mode setting finished, remains to do the button and his respective function

---
+++ js/darkMode.js
@@ -0,0 +1,25 @@
+document.querySelector(".template").classList.toggle("darkright_view")
+document.querySelector(".left-view").classList.toggle("darkModeleftview")
+document.querySelector(".active").classList.toggle("darkActive")
+document.querySelector(".circle__content").classList.toggle("darkcircle__content")
+document.querySelector(".backgroundCircle").classList.toggle("darkbackgroundCircle")
+document.querySelector(".box").classList.toggle("darkbox");
+document.querySelector(".right-view").classList.toggle("darkright_view");
+document.body.style.background = "#151515";
+
+
+document.querySelectorAll(".on").forEach(element => {
+    element.classList.toggle("darkActive")
+})
+
+document.querySelectorAll(".pomodoro__count").forEach(element => {
+    element.classList.toggle("darkbackgroundCircle")
+})
+
+document.querySelectorAll(".background__button").forEach(element =>{
+    element.classList.toggle("darkbackground__button")
+})
+
+document.querySelectorAll(".step").forEach(element =>{
+    element.classList.toggle("darkbox")
+})
--- js_templates/index.js
+++ js_templates/index.js
@@ -1,7 +1,12 @@
 import finalView from "./leftView.js"
+// import darkMode from "../js/darkMode.js"
 
 document.getElementById("left-view").innerHTML = finalView;
 
 var timer = document.createElement('script');
 timer.src = "../js/timer.js"
 document.body.append(timer);
+
+// var dark = document.createElement('script');
+// dark.src = "../js/darkMode.js"
+// document.body.append(dark);
