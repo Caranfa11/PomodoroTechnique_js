d7c1e201937b4500f57eade25070386629e35eed
Sistemas1 Europa <luiscaranfa1@gmail.com>
2023-07-12 10:05:58 -0400

    create rightview.js file with html code and added innerHTML in index.js

--- js_templates/index.js
+++ js_templates/index.js
@@ -1,7 +1,9 @@
 import finalView from "./leftView.js"
+import rightView from "./rightView.js"
 // import darkMode from "../js/darkMode.js"
 
 document.getElementById("left-view").innerHTML = finalView;
+document.getElementById("right-view").innerHTML = rightView
 
 var timer = document.createElement('script');
 timer.src = "../js/timer.js"
