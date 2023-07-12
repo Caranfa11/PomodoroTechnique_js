20099416ff8b9d8a1afd61cb973b681507f02c82
Sistemas1 Europa <luiscaranfa1@gmail.com>
2023-07-12 11:35:31 -0400

    Dark mode finished, added switch button and 0.25 transition to the whole page to give it a chill vibes

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,26 +1,32 @@
-document.querySelector(".template").classList.toggle("darkright_view")
-document.querySelector(".left-view").classList.toggle("darkModeleftview")
-document.querySelector(".active").classList.toggle("darkActive")
-document.querySelector(".circle__content").classList.toggle("darkcircle__content")
-document.querySelector(".backgroundCircle").classList.toggle("darkbackgroundCircle")
-document.querySelector(".box").classList.toggle("darkbox");
-document.querySelector(".right-view").classList.toggle("darkright_view");
-document.querySelector(".buttonsArea__box").classList.toggle("darkbuttonsArea__box")
-document.body.style.background = "#151515";
+const changeViewMode = () => {
+    document.querySelector(".template").classList.toggle("darkright_view")
+    document.querySelector(".left-view").classList.toggle("darkModeleftview")
+    document.querySelector(".active").classList.toggle("darkActive")
+    document.querySelector(".circle__content").classList.toggle("darkcircle__content")
+    document.querySelector(".backgroundCircle").classList.toggle("darkbackgroundCircle")
+    document.querySelector(".box").classList.toggle("darkbox");
+    document.querySelector(".right-view").classList.toggle("darkright_view");
+    document.querySelector(".buttonsArea__box").classList.toggle("darkbuttonsArea__box")
+    document.querySelector(".buttons__content").classList.toggle("darkbuttons__content")
+    document.body.style.background = "#151515";
 
 
-document.querySelectorAll(".on").forEach(element => {
-    element.classList.toggle("darkActive")
-})
+    document.querySelectorAll(".on").forEach(element => {
+        element.classList.toggle("darkActive")
+    })
 
-document.querySelectorAll(".pomodoro__count").forEach(element => {
-    element.classList.toggle("darkbackgroundCircle")
-})
+    document.querySelectorAll(".pomodoro__count").forEach(element => {
+        element.classList.toggle("darkbackgroundCircle")
+    })
 
-document.querySelectorAll(".background__button").forEach(element =>{
-    element.classList.toggle("darkbackground__button")
-})
+    document.querySelectorAll(".background__button").forEach(element =>{
+        element.classList.toggle("darkbackground__button")
+    })
 
-document.querySelectorAll(".step").forEach(element =>{
-    element.classList.toggle("darkbox")
-})
+    document.querySelectorAll(".step").forEach(element =>{
+        element.classList.toggle("darkbox")
+    })
+}
+
+
+document.getElementById("darkMode").addEventListener("click",changeViewMode)
