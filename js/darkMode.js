38bb7fcdf3f92cb7b7432b209e8db03e9e3f5551
Sistemas1 Europa <luiscaranfa1@gmail.com>
2023-07-14 16:22:03 -0400

    darkmode for ToDo was added

--- js/darkMode.js
+++ js/darkMode.js
@@ -4,7 +4,7 @@ const changeViewMode = (dark) => {
     document.querySelector(".active").classList.toggle("darkActive")
     document.querySelector(".circle__content").classList.toggle("darkcircle__content")
     document.querySelector(".backgroundCircle").classList.toggle("darkbackgroundCircle")
-    document.querySelector(".box").classList.toggle("darkbox");
+    // document.querySelector(".box").classList.toggle("darkbox");
     document.querySelector(".right-view").classList.toggle("darkright_view");
     document.querySelector(".buttonsArea__box").classList.toggle("darkbuttonsArea__box")
     document.querySelector(".buttons__content").classList.toggle("darkbuttons__content")
@@ -17,10 +17,21 @@ const changeViewMode = (dark) => {
     document.querySelectorAll(".background__button").forEach(element =>{
         element.classList.toggle("darkbackground__button")
     })
-    document.querySelectorAll(".step").forEach(element =>{
-        element.classList.toggle("darkbox")
-    })
+    // document.querySelectorAll(".step").forEach(element =>{
+    //     element.classList.toggle("darkbox")
+    // })
     document.querySelector('body').classList.toggle("dark");
+    document.querySelector(".box__todo").classList.toggle("darkbox__todo");
+    document.querySelector(".icon__todo").classList.toggle("darkicon__todo");
+    document.querySelectorAll(".item").forEach(element => {
+        element.classList.toggle("darkitem");
+    });
+    document.querySelectorAll(".item__delete").forEach(element => {
+        element.classList.toggle("darkitem__delete");
+    });
+    document.querySelectorAll(".item__edit").forEach(element => {
+        element.classList.toggle("darkitem__edit");
+    });
 }
 
 export {changeViewMode}
